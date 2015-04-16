e022117621fe3b4f9a03f8ba489b69ec96d9cb22
Eric D. Schabell <eric@schabell.org>
2015-04-16 12:15:21 -0400

    Updated to eap 6.4.

--- init.bat
+++ init.bat
@@ -14,7 +14,7 @@ set SRC_DIR=%PROJECT_HOME%installs
 set SUPPORT_DIR=%PROJECT_HOME%\support
 set PRJ_DIR=%PROJECT_HOME%\projects
 set BRMS=jboss-brms-6.1.0.GA-installer.jar
-set EAP=jboss-eap-6.4.0.CR2-installer.jar
+set EAP=jboss-eap-6.4.0-installer.jar
 set VERSION=6.1
 
 REM wipe screen.
